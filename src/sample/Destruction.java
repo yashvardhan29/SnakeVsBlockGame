807c09733a157e225ebf6c2adf315e1eff95efd6
Yashvardhan Singh <yashvardhan17123@iiitd.ac.in>
2018-12-02 20:30:45 +0530

    Added JDoc style comments to Token and it's subclasses and to class Wall.

--- src/sample/Destruction.java
+++ src/sample/Destruction.java
@@ -6,8 +6,15 @@ import javafx.scene.image.ImageView;
 import java.io.Serializable;
 
 public class Destruction extends Token implements Serializable {
+    /**
+     * Holds picture of destruction token(fireworks).
+     */
     transient ImageView icon;
 
+    /**
+     * Constructor for class.
+     * @param rx x co-ordinate of token.
+     */
     Destruction(int rx){
         super(rx);
 
@@ -17,6 +24,9 @@ public class Destruction extends Token implements Serializable {
         super.realg.getChildren().add(icon);
     }
 
+    /**
+     * Restores state on game being resumed.
+     */
     public void restore(){
         super.restore();
         Image img = new Image("file:src/fireworks.png");
